fa4b3ff5747c7b991a0e7e69147d17da22036e48
Rodrigo <rodrifs@Rodrigos-MacBook-Pro.local>
2020-05-25 16:43:19 +0200

    fixed case for min size == max size

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use clap::{App, Arg};
 use md5;
+use rayon::prelude::*;
 use std::fs::File;
 use std::io::Read;
 
@@ -27,9 +28,13 @@ fn main() {
         )
         .get_matches();
 
-    let path = matches.value_of("filepath").unwrap();
-    let etag = matches.value_of("etag").unwrap();
-
+    let (path, etag) = match (matches.value_of("filepath"), matches.value_of("etag")) {
+        (Some(p), Some(e)) => (p, e),
+        _ => {
+            eprintln!("Error: Missing parameters");
+            std::process::exit(0);
+        }
+    };
     let mut f = match File::open(path) {
         Ok(file) => file,
         Err(err) => {
@@ -45,39 +50,67 @@ fn main() {
         std::process::exit(0);
     }
     if etag.len() < 32 {
-        eprintln!("Error: Etag is not valid");
-        std::process::exit(0);
+        etag_error()
     }
 
-    guess_etag(buffer, etag);
+    let result = guess_etag_parallel(buffer, etag);
+    println!("{}", result);
 }
 
-fn guess_etag(buffer: Vec<u8>, etag: &str) {
-    let mut etag_parts: Vec<&str> = etag.split("-").collect();
-    if etag_parts.len() == 2 {
-        let chunks: i32 = etag_parts.pop().unwrap().parse().unwrap();
-        let file_size = buffer.len() / 1024 / 1024;
-        let min_sz = file_size / chunks as usize;
-        let max_sz = file_size / (chunks as usize - 1);
+fn etag_error() -> ! {
+    eprintln!("Error: Etag is not valid");
+    std::process::exit(0);
+}
 
-        for size in min_sz..max_sz {
-            let final_md5: Vec<u8> = buffer
-                .chunks(size * 1024 * 1024)
-                .flat_map(|b| md5::compute(b).to_vec())
-                .collect();
-            let calc_etag = md5::compute(final_md5);
-            if format!("{:x}-{}", calc_etag, chunks) == etag {
-                println!("match found");
-                std::process::exit(0);
+fn success_exit_early() -> ! {
+    println!("{}", true);
+    std::process::exit(0);
+}
+
+fn compute_simple(buffer: Vec<u8>, etag: &str, chunks: u32) -> bool {
+    let calc_etag = md5::compute(buffer);
+    match chunks {
+        1 => format!("{:x}-{}", calc_etag, chunks) == etag,
+        _ => format!("{:x}", calc_etag) == etag,
+    }
+}
+
+fn compute_concat(size: (usize, usize), chunks: u32, buffer: Vec<u8>, etag: &str) -> bool {
+    match (size.0..size.1).rev().par_bridge().find_any(|size| {
+        let final_md5: Vec<u8> = buffer
+            .par_chunks(size * 1024 * 1024)
+            .into_par_iter()
+            .flat_map(|b| md5::compute(b).to_vec())
+            .collect();
+        let calc_etag = md5::compute(final_md5);
+        if format!("{:x}-{}", calc_etag, chunks) == etag {
+            success_exit_early();
+        }
+        false
+    }) {
+        Some(_) => true,
+        None => false,
+    }
+}
+
+fn guess_etag_parallel(buffer: Vec<u8>, etag: &str) -> bool {
+    let mut etag_parts: Vec<&str> = etag.split("-").collect();
+    match etag_parts.len() {
+        1 => compute_simple(buffer, etag, 1),
+        2 => {
+            let chunks: u32 = match etag_parts.pop().unwrap().parse() {
+                Ok(c) if c > 1 => c,
+                _ => etag_error(),
+            };
+            let file_size = buffer.len() / 1024 / 1024;
+            let min_sz = file_size / chunks as usize;
+            let max_sz = file_size / (chunks as usize - 1);
+            if min_sz == max_sz {
+                compute_simple(buffer, etag, chunks)
+            } else {
+                compute_concat((min_sz, max_sz), chunks, buffer, etag)
             }
         }
-        eprintln!("Error: File is corrupted");
-    } else {
-        let calc_etag = md5::compute(buffer);
-        if format!("{:x}", calc_etag) == etag {
-            println!("match found");
-            std::process::exit(0);
-        }
-        eprintln!("Error: File is corrupted");
+        _ => etag_error(),
     }
 }
